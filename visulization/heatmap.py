0f638c92a829bed066146323b5e58c8341949498
Weikang Su <su_wei_kang@163.com>
2024-06-11 17:13:56 +0800

    updata

--- visulization/heatmap.py
+++ visulization/heatmap.py
@@ -2,6 +2,6 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
-gdf = gpd.read_file( "inter_crime_pred/visulization/heatmap_data_fake.geojson")
-gdf.plot(column="error", legend=True, cmap="OrRd")
-plt.savefig("inter_crime_pred/visulization/CHI_heatmap_opt.png")
+gdf = gpd.read_file( "inter_crime_pred/visulization/heatmap_data_CHI_our.geojson")
+gdf.plot(column="error", legend=True, cmap="OrRd", vmin=10, vmax=100)
+plt.savefig("inter_crime_pred/visulization/CHI_heatmap_our.png")
