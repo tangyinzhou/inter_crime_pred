764f4f5d63783916a4c46e6e5d0ee41b3c67c72f
Weikang Su <su_wei_kang@163.com>
2024-06-11 16:31:48 +0800

    优化后芝加哥热力图

--- visulization/heatmap.py
+++ visulization/heatmap.py
@@ -2,8 +2,6 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
-gdf = gpd.read_file(
-    "/home/tangyinzhou/inter_crime_pred/visulization/heatmap_data.geojson"
-)
+gdf = gpd.read_file( "inter_crime_pred/visulization/heatmap_data_fake.geojson")
 gdf.plot(column="error", legend=True, cmap="OrRd")
-plt.savefig("/home/tangyinzhou/inter_crime_pred/visulization/CHI_heatmap.png")
+plt.savefig("inter_crime_pred/visulization/CHI_heatmap_opt.png")
