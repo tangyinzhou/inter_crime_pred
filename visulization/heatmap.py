7ce61d77a614996868ac968ece06fe510c6553b5
Weikang Su <su_wei_kang@163.com>
2024-06-30 18:37:35 +0800

    Delete visulization directory

--- visulization/heatmap.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-
-gdf = gpd.read_file( "inter_crime_pred/visulization/heatmap_data_CHI_our.geojson")
-gdf.plot(column="error", legend=True, cmap="OrRd", vmin=10, vmax=100)
-plt.savefig("inter_crime_pred/visulization/CHI_heatmap_our.png")
